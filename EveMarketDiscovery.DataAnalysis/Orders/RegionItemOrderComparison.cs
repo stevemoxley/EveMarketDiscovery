2c128839932f0b81388e6fc729754fa2a923a400
stevemoxley <sdmfyc@gmail.com>
2018-06-19 16:41:51 -0500

    Stuff

---
+++ EveMarketDiscovery.DataAnalysis/Orders/RegionItemOrderComparison.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveMarketDiscovery.DataAnalysis.Orders
+{
+    public class RegionItemOrderComparison
+    {
+
+        public long RegionId { get; set; }
+
+        public long BaseRegionId { get; set; }
+
+        public List<ItemOrderComparison> ItemOrderComparisons = new List<ItemOrderComparison>();
+    }
+}
